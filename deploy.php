87d7dfe9c42ca0087fa0c5f9738d82456d432f43
Randriamazaoro <randriamazaorotantelinirina@gmail.com>
2019-02-26 18:16:30 +0300

    Deploy changes

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@ set('application', 'Ridge');
 set('repository', 'git@github.com:randriamazaoro/ridge.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', false); 
 
 // Shared files/dirs between deploys 
 add('shared_files', []);
