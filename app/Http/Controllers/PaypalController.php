a348ac9e75b6ebf2969a83cbcc92a60f57c8230a
Randriamazaoro <randriamazaorotantelinirina@gmail.com>
2019-03-01 14:32:23 +0300

    Paypal

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -7,16 +7,18 @@ use App\Invoice;
 use App\Program;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
 use PayPal\Api\Amount;
+use PayPal\Api\Details;
 use PayPal\Api\Item;
 use PayPal\Api\ItemList;
 use PayPal\Api\Payer;
 use PayPal\Api\Payment;
 use PayPal\Api\RedirectUrls;
+use PayPal\Api\ExecutePayment;
+use PayPal\Api\PaymentExecution;
 use PayPal\Api\Transaction;
-use PayPal\Auth\OAuthTokenCredential;
-use PayPal\Rest\ApiContext;
-use Srmklive\PayPal\Services\ExpressCheckout;
 
 class PaypalController extends Controller
 {
