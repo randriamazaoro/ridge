a1d594b7b960acc083e18fb64c1caa2dbdc9da1a
Randriamazaoro <randriamazaorotantelinirina@gmail.com>
2019-03-06 00:08:29 +0300

    Deploy

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -3,7 +3,7 @@
     <a
         href="{{ $url }}"
         class="{{ $color ?? 'is-primary' }} {{ $type ?? '' }}"
-        style="border: 1px solid transparent;font-size: 1rem; position: relative; border-width: 1px; cursor: pointer; padding: 7px 25px;background-color: #00ace8; border-color: transparent; color: #fff; border-radius: 290486px;"
+        style="border: 1px solid transparent;font-size: 1rem; position: relative; border-width: 1px; cursor: pointer; padding: 7px 25px;background-color: #00ace8; border-color: transparent; color: #fff; border-radius: 290486px;margin-top:5px;margin-bottom:5px;"
         target="_blank"
         >{{ $slot }}</a
     >
