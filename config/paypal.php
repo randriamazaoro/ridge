7c25a2b07c550209d24d753edbf468b32fcd85cf
Randriamazaoro <randriamazaorotantelinirina@gmail.com>
2019-03-07 17:31:23 +0300

    Paypal config

--- config/paypal.php
+++ config/paypal.php
@@ -1,13 +1,29 @@
 <?php
 
+if(env('PAYPAL_MODE') == 'live') {
 return [
-   'client_id' => env('PAYPAL_CLIENT_ID',''),
-   'secret' => env('PAYPAL_SECRET',''),
+   'client_id' => env('PAYPAL_CLIENT_ID_LIVE',''),
+   'secret' => env('PAYPAL_SECRET_LIVE',''),
    'settings' => array(
-       'mode' => env('PAYPAL_MODE','sandbox'),
+       'mode' => 'live',
        'http.ConnectionTimeOut' => 30,
        'log.LogEnabled' => true,
        'log.FileName' => storage_path() . '/logs/paypal.log',
        'log.LogLevel' => 'DEBUG'
    ),
 ];
+}
+
+if(env('PAYPAL_MODE') == 'sandbox') {
+    return [
+       'client_id' => env('PAYPAL_CLIENT_ID_SANDBOX',''),
+       'secret' => env('PAYPAL_SECRET_SANDBOX',''),
+       'settings' => array(
+           'mode' => 'sandbox',
+           'http.ConnectionTimeOut' => 30,
+           'log.LogEnabled' => true,
+           'log.FileName' => storage_path() . '/logs/paypal.log',
+           'log.LogLevel' => 'DEBUG'
+       ),
+    ];
+    }
